a31b33b46eed72793734b143b06533f9aa3d2e23
lazy1 <674194901@qq.com>
2025-01-16 20:22:18 +0800

    feat: add table init sql

--- internal/business/init.go
+++ internal/business/init.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"go-im/internal/business/api"
 	"go-im/pkg/protocol/pb"
+	"net"
 
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
@@ -20,7 +21,7 @@ func initLogger(version string) (logger *zap.Logger) {
 	return logger
 }
 
-func Start(ctx context.Context) error {
+func Start(ctx context.Context, rpcServerAddress string) error {
 	log = initLogger("debug")
 
 	// start business server
@@ -28,5 +29,15 @@ func Start(ctx context.Context) error {
 
 	pb.RegisterBusinessIntServer(server, &api.BusinessIntServer{})
 	pb.RegisterBusinessExtServer(server, &api.BusinessExtServer{})
+	listen, err := net.Listen("tcp", rpcServerAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Info("business rpc start", zap.String("address", rpcServerAddress))
+	err = server.Serve(listen)
+	if err != nil {
+		log.Error("serve error", zap.Error(err))
+	}
 	return nil
 }
